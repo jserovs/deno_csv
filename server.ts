c22aab889da279104977a895a10a00d7c3397c0c
Jevgenijs Serovs <jevgenijs.serovs@gmail.com>
2021-01-05 19:01:56 +0200

    procfile for heroku + test server

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
-// deno run --allow-net --allow-read standard-library.ts
-// Docs: https://deno.land/std/http
 import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve({ port: 8070 });
-console.log(`🦕 Deno server running at http://localhost:8070/ 🦕`);
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { args } = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = parse(args).port;
+
+const s = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+
 for await (const req of s) {
   req.respond({ body: "Hello from your first Deno server" });
 }
